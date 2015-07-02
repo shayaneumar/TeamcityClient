85d003c8488fd147fa55a4872e4f1ade7ac44c92
Shayane Umar <shayane.umar@jci.com>
2015-07-02 10:18:06 +0530

    initial commitwq

---
+++ client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoServer.Printer.Write();
+            Console.ReadLine();
+        }
+    }
+}
